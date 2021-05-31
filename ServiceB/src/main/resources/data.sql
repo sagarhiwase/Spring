7af9cd8e9490fb103ad4e7437f8260dfadcef255
sagarhiwase <43933732+sagarhiwase@users.noreply.github.com>
2021-05-31 15:46:23 +0530

    changes for eureka server

--- ServiceB/src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO SERVICE_B
-    (id,name) 
-VALUES
-    (1,'Lokesh'),
-    (2,'Deja'),
-    (3,'Caption');
--- ServiceC/src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO SERVICE_C
-    (sub_id,SUB_NAME) 
-VALUES
-    (1,'Math'),
-    (2,'Science'),
-    (3,'History');
-	
